868c35dad9dc11f0f64b69abf4688d9509de6398
ariefpratama135@gmail.com <ariefpratama135@gmail.com>
2023-09-28 23:05:06 +0700

    update actions

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -6,7 +6,24 @@ module.exports = class DevChallengeService extends cds.ApplicationService {
     }
 
     // TODO: Implement the bound action: assignQuestionsToTest  
-    assignQuestionsToTest(Test, id, questionsCount) {
+    async assignQuestionsToTest(pTest, id, questionsCount) {
+
+        const { Tests, Questions } = cds.entities
+
+        let query = SELECT.from(Questions).where`test_ID is null`.limit(questionsCount)
+        let questions = await cds.run(query)
+
+        if (questionsCount > questions.length) {
+            return 'Not enough questions available'
+        }
+
+        let updateMap = questions.map(q => UPDATE.entity(Questions).set({ test_ID: id.ID }).where({ ID: q.ID }))
+
+        console.log(updateMap)
+        // fire all at once
+        await Promise.all(updateMap.map(u => cds.run(u)))
+        await cds.db.run(UPDATE.entity(Tests).set({ modifiedAt: new Date() }).where({ ID: id.ID }))
+
         return `${questionsCount} question(s) successfully added to the test!`
     }
 } 
